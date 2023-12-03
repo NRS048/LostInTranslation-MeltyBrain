36e87c255a002a1e68c30114d948bf36ffbdc1af
Nathan Story <nathanrstory@gmail.com>
2023-12-03 18:19:40 -0500

    Update sketch_nov17a.ino

--- src/sketch_nov17a/sketch_nov17a.ino
+++ src/sketch_nov17a/sketch_nov17a.ino
@@ -6,11 +6,17 @@
 
 #define meltLED 23
 
+//#define SDDebug //if you want data on the teensy 4.1's sd card
+//#define SerialDebug //if you want data when connected to a computer
+#define DoHeading //if you want a heading light on your test stand
+
 KalmanFilter kf(0.0, 1.0, 0.1, 1.0);
 
-//CS for sd card
-File myFile;
-const int chipSelect = BUILTIN_SDCARD;
+#ifdef SDDebug
+  //CS for sd card
+  File myFile;
+  const int chipSelect = BUILTIN_SDCARD;
+#endif
 
 using namespace Melty::Navigation;
 
@@ -36,202 +42,212 @@ void setup() {
   delay(500);
   digitalWrite(meltLED, LOW);
 
-  // setup for the first accelerometer
-  Serial.println("Before construction 1");
-  accel1 = new Accelerometer::Accelerometer(10);
-  Serial.println("Before config 1");
-  accel1->configure();
-  Serial.println("Before full scale 1");
-  accel1->setFullScale(Accelerometer::Config::FullScale::G400);
-  Serial.println("Before ODR 1");
-  accel1->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
-  Serial.println("Before Calibrate 1");
-  accel1->calibrate();
+  #ifdef SerialDebug
+    // setup for the first accelerometer
+    Serial.println("Before construction 1");
+    accel1 = new Accelerometer::Accelerometer(10);
+    Serial.println("Before config 1");
+    accel1->configure();
+    Serial.println("Before full scale 1");
+    accel1->setFullScale(Accelerometer::Config::FullScale::G400);
+    Serial.println("Before ODR 1");
+    accel1->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
+    Serial.println("Before Calibrate 1");
+    accel1->calibrate();
 
-  Serial.println("Before construction 2");
-  accel2 = new Accelerometer::Accelerometer(9);
-  Serial.println("Before config 2");
-  accel2->configure();
-  Serial.println("Before full scale 2");
-  accel2->setFullScale(Accelerometer::Config::FullScale::G400);
-  Serial.println("Before ODR 2");
-  accel2->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
-  Serial.println("Before Calibrate 2");
-  accel2->calibrate();
+    Serial.println("Before construction 2");
+    accel2 = new Accelerometer::Accelerometer(9);
+    Serial.println("Before config 2");
+    accel2->configure();
+    Serial.println("Before full scale 2");
+    accel2->setFullScale(Accelerometer::Config::FullScale::G400);
+    Serial.println("Before ODR 2");
+    accel2->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
+    Serial.println("Before Calibrate 2");
+    accel2->calibrate();
 
-  Serial.print("Initializing SD card...");
+    #ifdef SDDebug
+      Serial.print("Initializing SD card...");
 
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD initialization failed!");
-    return;
-  }
-  Serial.println("SD initialization done.");
+      if (!SD.begin(chipSelect)) {
+        Serial.println("SD initialization failed!");
+        return;
+      }
+      Serial.println("SD initialization done.");
 
-  myFile = SD.open("datalogs/data.txt", FILE_WRITE);
+      myFile = SD.open("datalogs/data.txt", FILE_WRITE);
+      
+      // if the file opened okay, write to it:
+      if (myFile) {
+        Serial.print("Writing to data.txt...");
+        myFile.println("time, 1x, 1y, 1z, 1xgs, 1ygs, 1zgs, 2x, 2y, 2z, 2xgs, 2ygs, 2zgs, kalman45, rpm45, velocity, angle, LED");
+      // close the file:
+        myFile.close();
+        Serial.println("done.");
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+      }
+    #endif
+  #else
+    // setup for the first accelerometer
+    accel1 = new Accelerometer::Accelerometer(10);
+    accel1->configure();
+    accel1->setFullScale(Accelerometer::Config::FullScale::G400);
+    accel1->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
+    accel1->calibrate();
+
+    accel2 = new Accelerometer::Accelerometer(9);
+    accel2->configure();
+    accel2->setFullScale(Accelerometer::Config::FullScale::G400);
+    accel2->setOutputDataRate(Accelerometer::Config::DataRates::HZ_100);
+    accel2->calibrate();
+
+    #ifdef SDDebug
+      if (!SD.begin(chipSelect)) {
+        //Serial.println("SD initialization failed!");
+        return;
+      }
+
+      myFile = SD.open("datalogs/data.txt", FILE_WRITE);
+      
+      // if the file opened okay, write to it:
+      if (myFile) {
+        myFile.println("time, 1x, 1y, 1z, 1xgs, 1ygs, 1zgs, 2x, 2y, 2z, 2xgs, 2ygs, 2zgs, kalman45, rpm45, velocity, angle");
+      // close the file:
+        myFile.close();
+      } //else, error opening file
+    #endif
+  #endif
   
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to data.txt...");
-    myFile.println("time, 1x, 1y, 1z, 1xgs, 1ygs, 1zgs, 2x, 2y, 2z, 2xgs, 2ygs, 2zgs, kalman45, rpm45, velocity, angle, LED");
-	// close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void loop() {
   static long loopTimer = 0;
-  if (millis() - loopTimer > 10)
-  {
+  if (millis() - loopTimer > 10){
     loopTimer = millis();
     // put your main code here, to run repeatedly:
 
-    //Serial.println("Before accel read");
-    Accelerometer::AccelerometerReading a1 = accel1->read();
-    Accelerometer::AccelerometerReading a2 = accel2->read();
-    /*
-    Serial.printf("Valid: %d, x: %f, y: %f, z: %f\r\n",
-        reading.valid,
-        accel->convertToG(reading.accel_x),
-        accel->convertToG(reading.accel_y),
-        accel->convertToG(reading.accel_z));
-    Serial.printf("x: 0x%x, y: 0x%x, z: 0x%x\r\n",
-        reading.accel_x,
-        reading.accel_y,
-        reading.accel_z);
-    Serial.print(" 45* calc ");
-    kf.predict();
-    kf.update(accel->convertToG(((reading.accel_x+reading.accel_y)/2)/0.7071));
-    Serial.println(kf.getState());
-    */
-    /*
-    Serial.print(millis());
-    Serial.print(" ");
-    Serial.print(a1.accel_x);
-    Serial.print(" ");
-    Serial.print(a1.accel_y);
-    Serial.print(" ");
-    Serial.print(a1.accel_x);
-    Serial.print(" ");
-    Serial.print(accel1->convertToG(a1.accel_x));
-    Serial.print(" ");
-    Serial.print(accel1->convertToG(a1.accel_y));
-    Serial.print(" ");
-    Serial.print(accel1->convertToG(a1.accel_z));;
-    Serial.print(" ");
-    Serial.print(a2.accel_x);
-    Serial.print(" ");
-    Serial.print(a2.accel_y);
-    Serial.print(" ");
-    Serial.print(a2.accel_x);
-    Serial.print(" ");
-    Serial.print(accel2->convertToG(a2.accel_x));
-    Serial.print(" ");
-    Serial.print(accel2->convertToG(a2.accel_y));
-    Serial.print(" ");
-    Serial.println(accel2->convertToG(a2.accel_z));
-    */
+    Accelerometer::AccelerometerReading a1 = accel1->read(); //read the first accelerometer
+    Accelerometer::AccelerometerReading a2 = accel2->read(); //read the second accelerometer
+
+    A45 = ((abs(accel1->convertToG(a1.accel_x))+abs(accel1->convertToG(a1.accel_y)))/2)/0.7071; //convert the x and y values from accelerometer 1 into one adjusted value (allows a higher maximum measured g value)
+    B45 = ((abs(accel2->convertToG(a2.accel_x))+abs(accel2->convertToG(a2.accel_y)))/2)/0.7071; //convert the x and y values from accelerometer 2 into one adjusted value
     
+    combined45 = (A45+B45)/2; //finds the average of the two sensors' g value
 
-    myFile = SD.open("datalogs/data.txt", FILE_WRITE);
-    if (myFile) {
-      //Serial.print("Writing to data.txt...");
-      myFile.print(millis());
-      myFile.print(" ");
-      myFile.print(a1.accel_x);
-      myFile.print(" ");
-      myFile.print(a1.accel_y);
-      myFile.print(" ");
-      myFile.print(a1.accel_x);
-      myFile.print(" ");
-      myFile.print(accel1->convertToG(a1.accel_x));
-      myFile.print(" ");
-      myFile.print(accel1->convertToG(a1.accel_y));
-      myFile.print(" ");
-      myFile.print(accel1->convertToG(a1.accel_z));
-      myFile.print(" ");
-      myFile.print(a2.accel_x);
-      myFile.print(" ");
-      myFile.print(a2.accel_y);
-      myFile.print(" ");
-      myFile.print(a2.accel_x);
-      myFile.print(" ");
-      myFile.print(accel2->convertToG(a2.accel_x));
-      myFile.print(" ");
-      myFile.print(accel2->convertToG(a2.accel_y));
-      myFile.print(" ");
-      myFile.print(accel2->convertToG(a2.accel_z));
-      //myFile.print(" ");
-      //myFile.println(kf.getState()); //kalman filter datalog
-      //myFile.println(accel->convertToG(((reading.accel_x+reading.accel_y)/2)/0.7071));
-      //kf.predict();
-      //kf.update(accel->convertToG(((reading.accel_x+reading.accel_y)/2)/0.7071));
-      //myFile.print(kf.getState());
+    kf.predict(); //prepare kalman filter
+    kf.update(combined45); //run g value through a kalman filter
 
-      A45 = ((abs(accel1->convertToG(a1.accel_x))+abs(accel1->convertToG(a1.accel_y)))/2)/0.7071;
-      B45 = ((abs(accel2->convertToG(a2.accel_x))+abs(accel2->convertToG(a2.accel_y)))/2)/0.7071;
-      combined45 = (A45+B45)/2;
-      kf.predict();
-      kf.update(combined45);
+    rpm45 = sqrt(kf.getState()/(radiusCm * 11.18)) * 1000; //convert the filtered g value to RPM
 
-      myFile.print(" ");
-      myFile.print(kf.getState());
-      myFile.print(" ");
+    currentTime = millis(); //record time for calculations
+    if(firstLoop = true){ //if this is the first loop, the time since beginning will be so high, that it will claim it is at multiple thousand degrees.
+      deltaTimeSec = 0.011; // by setting the time to an accurate number, we keep the equation reasonable
+      firstLoop = false;
+    }else{
+      deltaTimeSec = (currentTime - pastTime)/1000; //find the time in seconds since the last iteration of the loop (should be ~10ms, but is usually closer to 11ms)
+    }
+    
+    velocity = rpm45 * 6; //convert rpm to degrees per second
+    angle = (velocity*(deltaTimeSec))+pastAngle; //fun the velocity through equation with the elapsed time and the past calculated angle to figure out our direction.
 
-      rpm45 = sqrt(kf.getState()/(radiusCm * 11.18)) * 1000;
+    if(angle > 360){ //keep the angle value between 0 and 360 degrees
+      angle -=360;
+    }else if(angle < 0){
+      angle += 360;
+    }
 
-      myFile.print(rpm45);
-      myFile.print(" ");
+    pastTime = currentTime; //save the values we just used fore the next equation
+    pastAngle = angle;
 
-      currentTime = millis();
-      if(firstLoop = true){
-        deltaTimeSec = 0.011;
-        firstLoop = false;
+    #ifdef DoHeading //enable at the top if you want to toggle the led when spinning.
+      if(angle >= 20 && angle < 70){
+        digitalWrite(meltLED, HIGH);
       }else{
-        deltaTimeSec = (currentTime - pastTime)/1000;
+        digitalWrite(meltLED, LOW);
       }
-      
-      velocity = rpm45 * 6; //convert rpm to degrees/second
-      angle = (velocity*(deltaTimeSec))+pastAngle;
-      Serial.print(angle);
+    #endif
+    
+    //debug options:
+    #ifdef SerialDebug
+      Serial.print(millis());
+      Serial.print(" ");
+      Serial.print(a1.accel_x);
+      Serial.print(" ");
+      Serial.print(a1.accel_y);
+      Serial.print(" ");
+      Serial.print(a1.accel_x);
+      Serial.print(" ");
+      Serial.print(accel1->convertToG(a1.accel_x));
+      Serial.print(" ");
+      Serial.print(accel1->convertToG(a1.accel_y));
+      Serial.print(" ");
+      Serial.print(accel1->convertToG(a1.accel_z));;
+      Serial.print(" ");
+      Serial.print(a2.accel_x);
+      Serial.print(" ");
+      Serial.print(a2.accel_y);
+      Serial.print(" ");
+      Serial.print(a2.accel_x);
+      Serial.print(" ");
+      Serial.print(accel2->convertToG(a2.accel_x));
+      Serial.print(" ");
+      Serial.print(accel2->convertToG(a2.accel_y));
+      Serial.print(" ");
+      Serial.println(accel2->convertToG(a2.accel_z));
+      Serial.print(" ");
+      Serial.print(kf.getState());
+      Serial.print(" ");
+      Serial.print(rpm45);
       Serial.print(" ");
       Serial.print(velocity);
       Serial.print(" ");
-      Serial.print(deltaTimeSec);
-      Serial.print(" ");
-      Serial.println(pastAngle);
-
-      if(angle > 360){
-        angle -=360;
-      }else if(angle < 0){
-        angle += 360;
+      Serial.print(angle);
+      Serial.println(" ");
+    #endif
+    
+    #ifdef SDDebug
+      myFile = SD.open("datalogs/data.txt", FILE_WRITE);
+      if (myFile) {
+        //Serial.print("Writing to data.txt...");
+        myFile.print(millis());
+        myFile.print(" ");
+        myFile.print(a1.accel_x);
+        myFile.print(" ");
+        myFile.print(a1.accel_y);
+        myFile.print(" ");
+        myFile.print(a1.accel_x);
+        myFile.print(" ");
+        myFile.print(accel1->convertToG(a1.accel_x));
+        myFile.print(" ");
+        myFile.print(accel1->convertToG(a1.accel_y));
+        myFile.print(" ");
+        myFile.print(accel1->convertToG(a1.accel_z));
+        myFile.print(" ");
+        myFile.print(a2.accel_x);
+        myFile.print(" ");
+        myFile.print(a2.accel_y);
+        myFile.print(" ");
+        myFile.print(a2.accel_x);
+        myFile.print(" ");
+        myFile.print(accel2->convertToG(a2.accel_x));
+        myFile.print(" ");
+        myFile.print(accel2->convertToG(a2.accel_y));
+        myFile.print(" ");
+        myFile.print(accel2->convertToG(a2.accel_z));
+        myFile.print(" ");
+        myFile.print(kf.getState());
+        myFile.print(" ");
+        myFile.print(rpm45);
+        myFile.print(" ");
+        myFile.print(velocity);
+        myFile.print(" ");
+        myFile.print(angle);
+        myFile.println(" ");
+        // close the file:
+        myFile.close();
       }
-      myFile.print(velocity);
-      myFile.print(" ");
-      myFile.print(angle);
-      myFile.print(" ");
-
-      pastTime = currentTime;
-      pastAngle = angle;
-
-      if(angle >= 20 && angle < 70){
-        digitalWrite(meltLED, HIGH);
-        myFile.print(1);
-      }else{
-        digitalWrite(meltLED, LOW);
-        myFile.print(0);
-      }
-
-      // close the file:
-      
-      myFile.println(" ");
-      
-      myFile.close();
-    }
-
-    //heading light calculations
+    #endif
   }
 }
