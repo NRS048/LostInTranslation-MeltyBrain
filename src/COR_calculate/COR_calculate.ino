75c5a15c46e7ca8046dc4058c40d5f9915dd80d3
Nathan Story <nathanrstory@gmail.com>
2023-12-31 21:00:27 -0500

    Add files via upload
    math to calculate the RPM directly from the 4 accelerometer values (2 accelerometers positioned at a 45-degree angle), accounting for an abnormal center of rotation.

---
+++ src/COR_calculate/COR_calculate.ino
@@ -0,0 +1,35 @@
+/*
+accel1->convertToG(a1.accel_x)
+accel1->convertToG(a1.accel_y)
+accel1->convertToG(a2.accel_x)
+accel1->convertToG(a2.accel_y)
+*/
+float a = -3;
+float b = 13.5;
+float c = 14.5;
+float d = -1.2;
+
+float rpm = 0;
+
+void setup() {
+  Serial.println(micro());
+  //(500/sqrt(1.118))
+  //sqrt(sq(a)+sq(b))
+  //sqrt( sq(sqrt(2)*(-a-b+c+d)+30) + 2 * sq(a-b-c+d) )
+  //sqrt(sqrt(sq(a)+sq(b))/(sqrt( sq(sqrt(2)*(-a-b+c+d)+30) + 2 * sq(a-b-c+d) )))
+  //sqrt(sq(c)+sq(d))
+  //sqrt( sq(sqrt(2)*(-a-b+c+d)-30) + 2 * sq(a-b-c+d) )
+  //sqrt(sqrt(sq(c)+sq(d))/(sqrt( sq(sqrt(2)*(-a-b+c+d)-30) + 2 * sq(a-b-c+d) )))
+  
+  //( sqrt(sqrt(sq(a)+sq(b))/(sqrt( sq(sqrt(2)*(-a-b+c+d)+30) + 2 * sq(a-b-c+d) ))) + sqrt(sqrt(sq(c)+sq(d))/(sqrt( sq(sqrt(2)*(-a-b+c+d)-30) + 2 * sq(a-b-c+d) ))) )
+
+
+  rpm = (500/sqrt(1.118)) * ( sqrt(sqrt(sq(a)+sq(b))/(sqrt( sq(sqrt(2)*(-a-b+c+d)+30) + 2 * sq(a-b-c+d) ))) + sqrt(sqrt(sq(c)+sq(d))/(sqrt( sq(sqrt(2)*(-a-b+c+d)-30) + 2 * sq(a-b-c+d) ))) );
+
+
+  Serial.println(rpm);
+  Serial.println(micro());
+}
+
+void loop() {
+}
